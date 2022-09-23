b5b6811dae618868705f67edea8cbd790b1b4108
Sureyya Akis <sureyyaabetul@gmail.com>
2022-09-22 21:13:24 -0700

    Create 595_Big_Countries.sql

---
+++ 595_Big_Countries.sql
@@ -0,0 +1,11 @@
+/* THIS IS ANSWER FOR 595.BIG COUNTRIES QUESTION ON LEETCODE */
+
+# Write your MySQL query statement below
+SELECT name,population,area
+FROM World
+WHERE area >= 3000000 or population >= 25000000;
+
+
+
+
+
