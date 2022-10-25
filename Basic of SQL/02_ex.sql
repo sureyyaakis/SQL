557fea19f92d06a483ab40492c0b252cf1850dc7
Sureyya Akis <sureyyaabetul@gmail.com>
2022-10-24 20:01:05 -0700

    Update 02_ex.sql

--- Basic of SQL/02_ex.sql
+++ Basic of SQL/02_ex.sql
@@ -1,1 +1,9 @@
+# Types of Operators : Arithmetic(*/+-)(left to right), Strings,   
+# NULL valuse Null 
+# DISTINCT keyword eliminate the duplication 
 
+SELECT empno AS ID, name AS "Employee Name" FROM emp_tab;
+
+SELECT name||' belongs to '||deptno||' department' AS " Employee Department" FROM emp_tab;
+
+SELECT DISTINCT deptno FROM emp_tab;
