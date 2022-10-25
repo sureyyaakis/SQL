53364a19b6fb58cf6335eb313f8ca895b705400f
Sureyya Akis <sureyyaabetul@gmail.com>
2022-10-24 19:50:43 -0700

    Update 01_ex.sql

--- Basic of SQL/01_ex.sql
+++ Basic of SQL/01_ex.sql
@@ -1,4 +1,5 @@
-#This tables select and brings the empno, and name table
+# This tables select and brings the empno, and name table
+# Upper Case for the Keywords, lower case for the rest 
 
 SELECT * FROM emp_tab;
 SELECT empno, name FROM emp_tab;
