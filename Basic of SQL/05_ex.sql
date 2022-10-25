cab2bdf0191317b93ba7802dd0b0d4c430474723
Sureyya Akis <sureyyaabetul@gmail.com>
2022-10-24 20:36:41 -0700

    Create 05_ex.sql

---
+++ Basic of SQL/05_ex.sql
@@ -0,0 +1,5 @@
+# Conditions order 
+
+SELECT empno,name,deptno,salary FROM emp_tab WHERE deptno=30 OR deptno=40 AND salary>3000;
+
+SELECT empno,name,deptno,salary FROM emp_tab WHERE (deptno=30 OR deptno=40) AND salary>3000;
